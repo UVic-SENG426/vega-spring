a7a3dfafbb80e4cf69ffe56f788d5afc3aa056f5
Russell Waterhouse <Russell.L.Waterhouse@gmail.com>
2022-06-27 20:51:06 -0700

    setup skeleton of secret storage/retrieval workflow

--- src/main/java/com/uvic/venus/controller/VaultController.java
+++ src/main/java/com/uvic/venus/controller/VaultController.java
@@ -1,4 +1,26 @@
 package com.uvic.venus.controller;
 
+import com.uvic.venus.model.Secret;
+import com.uvic.venus.repository.SecretDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
 public class VaultController {
+
+    @Autowired
+    SecretDAO secretDAO;
+
+    @RequestMapping(value ="/secrets", method = RequestMethod.GET)
+    public ResponseEntity<?> getSecrets(@RequestParam String param1, @RequestParam String param2){
+
+        List<Secret> secretList = secretDAO.findAll();
+        return ResponseEntity.ok("OK: secrets returned successfully");
+    }
+
+//    TODO: other endpoints
 }
